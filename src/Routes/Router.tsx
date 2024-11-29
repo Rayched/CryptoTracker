c3dd649e90ff3e2156f292cb4e5e2277cdc38540
Rayched <ny2401@naver.com>
2024-11-29 23:43:41 +0900

    crypto-tracker_mk3 작업 완료.. 배포 이슈 발생

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -4,7 +4,7 @@ import Detail from "./Detail";
 
 function Routers(){
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/:coinID/*" element={<Detail />}/>
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider, useQueryClient } from 'react-query';
+import { QueryClient, QueryClientProvider} from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
